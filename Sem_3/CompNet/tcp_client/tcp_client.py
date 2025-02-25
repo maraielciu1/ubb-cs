d97cd8260fc1d59e96066ff8e79e10b09be2433c
maraielciu1 <mara.ielciu@stud.ubbcluj.ro>
2025-02-25 17:09:03 +0200

    comp net updated

--- Sem_3/CompNet/tcp_client/tcp_client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 7777))
-s.send(b"Salut")
-print(s.recv(10))
-s.close()
--- Sem_3/CompNet/udp_server.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("0.0.0.0", 5555))
-buff, addr = s.recvfrom(10)
-print(buff.decode())  # Decode bytes to string before printing
-s.sendto(b"hello", addr)  # Send bytes, not string
-
-
